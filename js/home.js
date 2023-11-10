12156fdca601d183208f6126c03ab3d44efc73bf
ashiq611 <72414858+ashiq611@users.noreply.github.com>
2023-11-10 20:06:30 +0600

    deploy

---
+++ js/home.js
@@ -0,0 +1,8 @@
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
